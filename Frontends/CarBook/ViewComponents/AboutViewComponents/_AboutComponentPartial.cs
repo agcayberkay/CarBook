fc937cd1c398ab440885060de1db1848a3c10765
pc <pc@DESKTOP-PJJV610>
2025-06-25 00:08:05 +0300

    Hakkımda sayfası partial işlemi yapıldı.

---
+++ Frontends/CarBook/ViewComponents/AboutViewComponents/_AboutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.ViewComponents.AboutViewComponents
+{
+    public class _AboutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
