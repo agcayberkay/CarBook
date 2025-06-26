4b98249c869dc6cfe7a0009b958de80a6490b0c2
pc <pc@DESKTOP-PJJV610>
2025-06-26 16:21:36 +0300

    Hizmetler tamamlandı ve Car page düzenlendi.

---
+++ Frontends/CarBook/Controllers/CarWithBrandsController.cs
@@ -0,0 +1,30 @@
+﻿using CarBook.Dto.CarWithBrandsDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.Controllers
+{
+    public class CarWithBrandsController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CarWithBrandsController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7010/api/Cars/GetCarWithBrand");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandsDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
+
---
+++ Frontends/CarBook/Controllers/ServiceController.cs
@@ -0,0 +1,30 @@
+﻿using CarBook.Dto.ServiceDtos;
+using CarBook.Dto.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ServiceController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task< IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7010/api/Services");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ServiceResultDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
