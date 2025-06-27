0e845e7960645087c8263de8020bf1a3f0a4b90e
pc <pc@DESKTOP-PJJV610>
2025-06-27 17:26:34 +0300

    Contact ve FooterAdress controllerları eklendi.

---
+++ Frontends/CarBook/Controllers/ContactController.cs
@@ -0,0 +1,38 @@
+﻿
+using CarBook.Dto.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+           return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateContactDto contactDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(contactDto);
+            contactDto.SendDate = DateTime.Now; 
+            var content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7010/api/Contacts", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+            return View();
+        }
+    }
+}
