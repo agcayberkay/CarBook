97c79cc60d33224f7599f5256bb0492fc9afe1da
pc <pc@DESKTOP-PJJV610>
2025-07-24 12:28:32 +0300

    Tag Cloudslar düzenlendi. APİ consume edildi.

--- Frontends/CarBook/ViewComponents/BlogViewComponents/_BlogDetailsTagCloudComponentPartial.cs
+++ Frontends/CarBook/ViewComponents/BlogViewComponents/_BlogDetailsTagCloudComponentPartial.cs
@@ -16,12 +16,13 @@ namespace CarBook.ViewComponents.BlogViewComponents
 
         public async Task<IViewComponentResult> InvokeAsync(int id)
         {
+            ViewBag.BlogId = id;
             var client = _httpClientFactory.CreateClient();
-            var response = await client.GetAsync($"https://localhost:7010/api/TagClouds/" + id);
+            var response = await client.GetAsync($"https://localhost:7010/api/TagClouds/GetTagCloudsByBlogId?blogId=" + id);
             if (response.IsSuccessStatusCode)
             {
                 var jsonData = await response.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<GetTagCloudsByBlogIdDto>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<GetTagCloudsByBlogIdDto>>(jsonData);
                 return View(values);
             }
             return View();
