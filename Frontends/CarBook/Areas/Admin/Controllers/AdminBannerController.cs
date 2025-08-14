69ff3c3c4911e7a50b4ab0c2c014d465a485c5ff
pc <pc@DESKTOP-PJJV610>
2025-08-14 20:18:43 +0300

    Area eklendi. Brand işlemleri tamamlandı. Banner işlemlerine başlanıldı.

---
+++ Frontends/CarBook/Areas/Admin/Controllers/AdminBannerController.cs
@@ -0,0 +1,67 @@
+﻿using CarBook.Dto.BannerDtos;
+using CarBook.Dto.BrandDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminBanner")]
+    public class AdminBannerController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminBannerController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7010/api/Banners");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBannerDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("CreateBanner")]
+        public IActionResult CreateBanner()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("CreateBanner")]
+        public async Task<IActionResult> CreateBanner(CreateBannerDto createBannerDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createBannerDto);
+            StringContent content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7010/api/Banners", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index","AdminBanner",new {area ="Admin"});
+            }
+            return View();
+        }
+
+        [Route("RemoveBanner/{id}")]
+        public async Task<IActionResult> RemoveBanner(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.DeleteAsync($"https://localhost:7010/api/Banners?id=" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminBanner", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
---
+++ Frontends/CarBook/Controllers/AdminBrandController.cs
@@ -0,0 +1,89 @@
+﻿using CarBook.Dto.BrandDtos;
+using CarBook.Dto.FeaturesDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.Controllers
+{
+    public class AdminBrandController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminBrandController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7010/api/Brands");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBrandDtos>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateBrand()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBrand(CreateBrandDto createBrandDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createBrandDto);
+            StringContent content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7010/api/Brands", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> RemoveBrand(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.DeleteAsync($"https://localhost:7010/api/Brands?id=" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateBrand(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync($"https://localhost:7010/api/Brands/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateBrandDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateBrand(UpdateBrandDto updateBrandDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateBrandDto);
+            StringContent content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var response = await client.PutAsync("https://localhost:7010/api/Brands", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
